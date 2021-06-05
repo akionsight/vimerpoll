777c3876dd537f6cb2c0d67d362e55e2191a6972
Harsh Pandey <akionsight@gmail.com>
2021-06-05 02:27:06 -0400

    more commits, with most changes in the frontend, cors issues fixed

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,49 @@
 const add_poll_option = document.getElementById('add-poll-option')
+const remove_poll_option = document.getElementById('remove-poll-option')
 const poll_option_div = document.getElementById('poll-option-div')
 const submit_button = document.getElementById('submit-button')
 const poll_options = document.getElementsByClassName('poll-option')
 const poll_question = document.getElementById('poll-question')
+const main_area = document.getElementById('main')
+const body = document.getElementById('body')
+function create_popup (type, message) { 
+
+  if (type === 'warning') {
+    console.log('here')
+    const nested_elementelement = document.createElement('div')
+    element.setAttribute('class', 'alert alert-warning alert-text alert-dismissible')
+    element.innerHTML = message
+    body.insertBefore(element, main_area)
+  } 
+
+  if (type == 'success') {
+    const element = document.createElement('div')
+    element.setAttribute('class', 'alert alert-success alert-text alert-dismissible col-sm-4 col-sm-offset-3')
+    element.innerHTML = message
+  }
+
+}
+
+create_popup('warning', 'testing create popup')
+
 
 
 add_poll_option.onclick = () => {
     const element = document.createElement('input')
+    const poll_option_length = poll_options.length
     element.setAttribute('class', 'poll-option form-control')
     element.setAttribute('type', 'text')
+    element.setAttribute('id', `poll-option-${poll_option_length + 1}`)
     poll_option_div.appendChild(element)
 }
+
+remove_poll_option.onclick = () => {
+  console.log('remove poll option clicked')
+  const poll_option_length = poll_options.length
+  const element_to_remove = document.getElementById(`poll-option-${poll_option_length}`)
+  element_to_remove.remove()
+}
+
 
 submit_button.onclick = async function () {
     let options = []
@@ -35,4 +68,8 @@ submit_button.onclick = async function () {
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data) // body data type must match "Content-Type" header
       });
+      console.log(response)
 }
+
+
+// alert alert-warning alert-text alert-dismissible
